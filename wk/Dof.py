42f30fce8c3623d2240b9ab37f1e3797e6955b5c
yumouse <kuang@zju.edu.cn>
2020-03-14 23:04:34 +0800

    Algorithm 1 and 2 done.

---
+++ wk/Dof.py
@@ -0,0 +1,46 @@
+# Decomposition of Harmony
+# Input: User ui'sset of values V [-1,1]d
+#        Privacy budget epsilon
+# Output: Perturbed set of values Vstar
+import sys
+sys.path.append('../src')
+import gen
+import random
+import math
+import numpy as np
+
+# V-> User Ui's set of values
+# e-> epsilon
+def dof(V,e):
+    # step 1
+    Vstar = np.zeros(V.shape)
+    # step 2
+    j = random.randint(0,len(V)-1)
+    v = V[j]
+    # step 3 Discretization
+    if random.random() < (1+v)/2:
+        vstar = 1
+    else:
+        vstar = -1
+    # step 4 Pertubation
+    if random.random() < (math.exp(e)/(1+math.exp(e))):
+        vstar = vstar
+    else:
+        vstar = -vstar
+    # step 5 Calibration
+    vstar = vstar * ((math.exp(e)+1)/(math.exp(e)-1)) * len(V)
+    # step 6
+    Vstar[j] = vstar
+    # step 7
+    return Vstar
+
+if __name__ == '__main__':
+    tmpdata = gen.generator(10,3).gen()
+    ary = np.array(tmpdata)
+    print ary.shape
+    index = random.randint(0,9)
+    V = ary[index,:,1]
+    print V.shape
+    Vstar = dof(V,random.random())
+    print Vstar
+
---
+++ wk/VPP.py
@@ -0,0 +1,26 @@
+# Value Perturbation Primitive
+# Input:    Value v of a KV pair
+#           Privacy budget epsilon
+# Output:   VPP(v,e) is the perturbed value vstar
+import random
+import math
+def VPP(v,e):
+    # step 1 Discretization
+    if random.random() < (1+v)/2:
+        vstar = 1
+    else:
+        vstar = -1
+    # step 1 Perturbation
+    if random.random() < (math.exp(e)/(1+math.exp(e))):
+        vstar = vstar
+    else:
+        vstar = -vstar
+    # Return
+    return vstar
+
+if __name__ == '__main__':
+    v = random.random()*2-1
+    e = random.random()
+    print "v is %f and epsilon is %f" %  (v,e)
+    vstar = VPP(v,e)
+    print "vstar is %f" % vstar
