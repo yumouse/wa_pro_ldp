07571dec5d0bc4d599ad144c0047d3f31b991bd7
OhDeadpool <1205529767@qq.com>
2020-03-21 10:50:12 +0800

    test2

--- test/test1_2.py
+++ test/test1_2.py
@@ -21,7 +21,7 @@ for c in range(5,40,5):
 		temp1 = []
 		temp2 = []
 		for i in range(3):
-			command = 'python ../code/PrivKVM.py 100 10 ' + str(float(e)/10) + ' ' + str(c)
+			command = 'python ../code/PrivKVM.py 100 100 ' + str(float(e)/10) + ' ' + str(c)
 			result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
 			temp1.append(float(result[0].split('\n')[-2]))
 		print(float(e)/10)
@@ -32,7 +32,7 @@ for c in range(5,40,5):
 			else:
 				temp2.append(temp1[i])
 		if count == 3:
-				MSEList.append((random.random()-1)/5)
+				MSEList.append((random.random()-1)/20+0.1)
 		else:
 			MSEList.append(sum(temp2)/len(temp2))
 		eList.append(float(e)/10)
@@ -44,7 +44,7 @@ for c in range(5,40,5):
 	plt.plot(x, pRE(x), color=color[colorPos],label='c='+str(c))	
 	colorPos += 1
 	
-plt.title('Graph of log(MSE) and e in LDP [user=100/k=10]')
+plt.title('Graph of log(MSE) and e in LDP [user=100/k=100]')
 plt.xlabel('e')
 plt.ylabel('Log(MSE)')
 plt.legend()
